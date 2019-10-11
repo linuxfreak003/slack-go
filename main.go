aced8af701c290b92d3ea1ac27c97d9c8162051b
Jared Chapman <jared.chapman@tcnbroadcasting.com>
2019-10-10 21:37:28 -0600

    messy, but working

--- main.go
+++ main.go
@@ -2,46 +2,81 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/bluele/slack"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	var message, token, channel string
+	var slackToken string
+
+	home, _ := os.UserHomeDir()
+	tokenFile, err := ioutil.ReadFile(fmt.Sprintf("%s/.config/go-pomodoro/slack-token", home))
+	if err == nil {
+		slackToken = string(tokenFile)
+	}
+
+	if slackToken == "" {
+		slackToken = os.Getenv("SLACK_TOKEN")
+	}
+
+	slackToken = strings.TrimSpace(slackToken)
+
+	flag.StringVar(&message, "m", "", "Message to send")
+	flag.StringVar(&token, "t", slackToken, "Slack API Token")
+	flag.StringVar(&channel, "c", "", "Slack Channel to send to")
+	flag.Parse()
+
 	info, err := os.Stdin.Stat()
 	if err != nil {
 		panic(err)
 	}
 
+	if channel == "" {
+		fmt.Println("Error: No channel specified")
+		return
+	}
+
+	if message != "" {
+		api := slack.New(token)
+		err = api.ChatPostMessage(channel, message, &slack.ChatPostMessageOpt{
+			AsUser: true,
+		})
+		if err != nil {
+			log.Warnf("slack message not sent: %v", err)
+		}
+		return
+	}
+
 	if info.Mode()&os.ModeCharDevice != 0 || info.Size() <= 0 {
-		fmt.Println("The command is intended to work with pipes.")
-		fmt.Println("Usage: fortune | gocowsay")
+		fmt.Println("Error: No message given")
 		return
 	}
 
 	reader := bufio.NewReader(os.Stdin)
-	var output []rune
 
+	var build strings.Builder
 	for {
 		input, _, err := reader.ReadRune()
 		if err != nil && err == io.EOF {
 			break
 		}
-		output = append(output, input)
+		build.WriteRune(input)
+	}
+	message = build.String()
+	if message == "" {
+		fmt.Println("Error: No message given")
 	}
 
-	for j := 0; j < len(output); j++ {
-		fmt.Printf("%c", output[j])
-	}
-
-	token := "xoxp-499911687078-498709891189-738830943330-af3669e4107f8ba4fe1661f61e0ca40e"
-	message := "Hello World!"
-	channelName := "public-ip"
 	api := slack.New(token)
-	err := api.ChatPostMessage(channelName, message, &slack.ChatPostMessageOpt{
+	err = api.ChatPostMessage(channel, message, &slack.ChatPostMessageOpt{
 		AsUser: true,
 	})
 	if err != nil {
